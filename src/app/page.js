e36d6baf5463fef93d3582878d4d5880585ad99d
BungaZaskiaHabibah <bzaskia78@gmail.com>
2026-07-20 17:44:29 +0700

    Fix homepage data fetching for Vercel

--- src/app/page.js
+++ src/app/page.js
@@ -1,64 +1,47 @@
+import { prisma } from "@/lib/prisma";
 import ProductList from "@/components/ProductList";
 import ProductForm from "@/components/ProductForm";
 import DashboardCard from "@/components/dashboard/DashboardCard";
 
-
 async function getProducts() {
-  const res = await fetch("http://localhost:3000/api/products", {
-    cache: "no-store",
+  return await prisma.product.findMany({
+    orderBy: {
+      id: "asc",
+    },
   });
-
-  if (!res.ok) {
-    throw new Error("Gagal mengambil data produk.");
-  }
-
-  return res.json();
 }
 
 export default async function Home() {
   const products = await getProducts();
 
-  const totalStock = products.reduce((total, product) => {
-  return total + product.stock;
-}, 0);
+  const totalStock = products.reduce(
+    (total, product) => total + product.stock,
+    0
+  );
 
-const outOfStock = products.filter((product) => product.stock === 0).length;
-
-
+  const outOfStock = products.filter(
+    (product) => product.stock === 0
+  ).length;
 
   return (
     <main style={{ padding: "30px" }}>
       <h1>Daftar Produk</h1>
 
       <div
-  style={{
-    display: "flex",
-    gap: "20px",
-    margin: "20px 0",
-    flexWrap: "wrap",
-  }}
->
-  <DashboardCard
-    title="Total Produk"
-    value={products.length}
-  />
-
-  <DashboardCard
-  title="Total Stok"
-  value={totalStock}
-/>
-
- <DashboardCard
-  title="Produk Habis"
-  value={outOfStock}
-/>
-</div>
+        style={{
+          display: "flex",
+          gap: "20px",
+          margin: "20px 0",
+          flexWrap: "wrap",
+        }}
+      >
+        <DashboardCard title="Total Produk" value={products.length} />
+        <DashboardCard title="Total Stok" value={totalStock} />
+        <DashboardCard title="Produk Habis" value={outOfStock} />
+      </div>
 
       <ProductForm />
-
       <ProductList products={products} />
     </main>
-
-    
   );
 }
